a496b85814bc59a36f4a5dc690213ff66e5aabcb
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2021-01-30 16:15:24 +0000

    added phase switch and list of marbles to box

---
+++ Assets/BoxContainer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxContainer : MonoBehaviour
+{
+    public List<GameObject> listOfGO = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/InGameManager.cs
+++ Assets/InGameManager.cs
@@ -9,26 +9,82 @@ public class InGameManager : MonoBehaviour
     public float currentTime;
     public bool gameStarted = false;
 
+    public int roundNum = 0;
+    public Text roundInfoText;
     public Text countdownText;
+
+    int phase = 0;
+
+    public List<GameObject> listOfGO = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        currentTime = Time.time;
-        countdownText.text = (startTime - (int)currentTime).ToString();
+        RoundStart();
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime = Time.time;
+        currentTime += Time.deltaTime;
 
+        switch (phase) {
+            case 0:
+                RoundStart();
+                break;
+            case 1:
+                WaitPhase();
+                break;
+            case 2:
+                GamePhase();
+                break;
+            case 3:
+                PlayerSelectionPhase();
+                break;
+        }
 
-        if (currentTime > startTime) {
+    }
+
+    void RoundStart() {
+
+        currentTime = 0;
+        countdownText.text = (startTime - (int)currentTime).ToString();
+        roundInfoText.text = "Round Starts: ";
+        roundNum++;
+        phase = 1;
+        // tell the rats to go to for marbles...reachedbox = false  
+    }
+
+    void RoundOver() { 
+       
+        //player has made selections
+        //  call round start
+    }
+    void WaitPhase() {
+
+        // check if game ready to start
+        if (currentTime > startTime)
+        {
             gameStarted = true;
             countdownText.text = " ";
+            roundInfoText.text = "Round: " + roundNum.ToString();
+            phase = 2;
         }
-        else {
+        else
+        {
+            // set text to be countdown value
             countdownText.text = (startTime - (int)currentTime).ToString();
         }
+    }
+    void GamePhase() {
+
+        // Debug for checking the round start code works... change this to be called after player has made choices
+        if (currentTime > 20)
+        {
+            phase = 3;
+        }
+    }
+
+    void PlayerSelectionPhase() {
+        roundInfoText.text = "Find the Green Marble!";
     }
 }
