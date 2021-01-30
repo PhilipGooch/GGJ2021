9d228693daa13eda6443622b7151909b1144ae3e
PhilipGooch <philip.daniel.gooch@gmail.com>
2021-01-30 20:07:19 +0000

    changed level

---
+++ Assets/RatManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatManager : MonoBehaviour
+{
+    public List<GameObject> rats;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
